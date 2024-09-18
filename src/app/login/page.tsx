6b598fa4e73167694ae62dcb036e6fd2faaca510
dylanmartin0930 <dyma930@gmail.com>
2024-09-18 16:40:36 -0700

    Signup & Login Complete
    implemented Signup & login pages alongside their respective API
    endpoints.
    
    Known Bugs:
    - once user is logged in, they cans still return to /login & /signup
    - no way for users to Sign out
    
    Need:
    - controllers
    - Signout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,46 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function LoginPage() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
 
-  const onLogin = async () => {};
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login success", response.data);
+      toast.success("Login successful");
+      router.push("/profile");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Login</h1>
+      <h1>{loading ? "processing" : "Login"}</h1>
 
       <hr />
       {/* Email */}
@@ -41,7 +67,7 @@ export default function LoginPage() {
         onClick={onLogin}
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-white"
       >
-        Login
+        {buttonDisabled ? "Missing Field" : "Login"}
       </button>
       <Link href="/signup">Visit Signup Page </Link>
     </div>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,21 +1,52 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function SignupPage() {
+  const router = useRouter();
+
   const [user, setUser] = React.useState({
     email: "",
     password: "",
     username: "",
   });
 
-  const onSignup = async () => {};
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup success", response.data);
+      router.push("/login");
+    } catch (error: any) {
+      toast.error(error.message);
+      console.log(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // dependent on user
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Sign Up</h1>
+      <h1>{loading ? "Processing" : "Sign Up"}</h1>
 
       <hr />
       {/* Username  */}
@@ -52,7 +83,7 @@ export default function SignupPage() {
         onClick={onSignup}
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-white"
       >
-        Sign Up
+        {buttonDisabled ? "Missing Field" : "Sign Up"}
       </button>
       <Link href="/login">Visit Login Page </Link>
     </div>
